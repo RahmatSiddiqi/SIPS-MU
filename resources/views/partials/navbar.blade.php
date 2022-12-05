12423e4f61b30a625d4e22585d873e763e5d0a79
MuhammadRifqi24 <muhammadrifqi24062002@gmail.com>
2022-12-05 19:44:36 +0700

    Dash

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-dark">
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav mx-auto">
+        <div class="d-flex">
+            <div class="brand">
+                <img src="{{ asset('images/logo.png') }}" alt="" class="ml-auto" width="35px">
+            </div>
+            <div>
+                <!-- Navbar Search -->
+                <h3 class="text-center text-white">SISTEM INFORMASI PERIZINAN SANTRI</h3>
+            </div>
+            <div class="user">
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-circle" width="40"
+                    height="40" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                    stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                    <circle cx="12" cy="12" r="9"></circle>
+                    <circle cx="12" cy="10" r="3"></circle>
+                    <path d="M6.168 18.849a4 4 0 0 1 3.832 -2.849h4a4 4 0 0 1 3.834 2.855"></path>
+                </svg>
+            </div>
+        </div>
+
+    </ul>
+</nav>
---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,126 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4 mt-5">
+
+
+    <!-- Sidebar Menu -->
+    <nav class="m-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <!-- Add icons to the links using the .nav-icon class
+            with font-awesome or any other icon font library -->
+            <li class="nav-item mb-2 first">
+                <a href="pages/calendar.html" class="nav-link">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 icon icon-tabler icon-tabler-file-description"
+                        width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                        fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                        <path d="M14 3v4a1 1 0 0 0 1 1h4"></path>
+                        <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z"></path>
+                        <path d="M9 17h6"></path>
+                        <path d="M9 13h6"></path>
+                    </svg>
+                    <p>
+                        Informasi Akun Now
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item mb-2">
+                <a href="pages/calendar.html" class="nav-link">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 icon icon-tabler icon-tabler-dashboard"
+                        width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                        fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                        <circle cx="12" cy="13" r="2"></circle>
+                        <line x1="13.45" y1="11.55" x2="15.5" y2="9.5"></line>
+                        <path d="M6.4 20a9 9 0 1 1 11.2 0z"></path>
+                    </svg>
+                    <p>
+                        Home
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item mb-2">
+                <a href="pages/calendar.html" class="nav-link">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 icon icon-tabler icon-tabler-file-description"
+                        width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                        fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                        <path d="M14 3v4a1 1 0 0 0 1 1h4"></path>
+                        <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z"></path>
+                        <path d="M9 17h6"></path>
+                        <path d="M9 13h6"></path>
+                    </svg>
+                    <p>
+                        Data Santri
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item mb-2">
+                <a href="pages/calendar.html" class="nav-link">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 icon icon-tabler icon-tabler-file-description"
+                        width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                        fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                        <path d="M14 3v4a1 1 0 0 0 1 1h4"></path>
+                        <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z"></path>
+                        <path d="M9 17h6"></path>
+                        <path d="M9 13h6"></path>
+                    </svg>
+                    <p>
+                        Data Ustadz
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item mb-2">
+                <a href="pages/calendar.html" class="nav-link">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 icon icon-tabler icon-tabler-file-description"
+                        width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                        fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                        <path d="M14 3v4a1 1 0 0 0 1 1h4"></path>
+                        <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z"></path>
+                        <path d="M9 17h6"></path>
+                        <path d="M9 13h6"></path>
+                    </svg>
+                    <p>
+                        Perizinan
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item mb-2">
+                <a href="{{ route('laporan') }}" class="nav-link  {{ Route::is('laporan') ? 'bg-primary' : ''}}">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 icon icon-tabler icon-tabler-file-description"
+                        width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                        fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                        <path d="M14 3v4a1 1 0 0 0 1 1h4"></path>
+                        <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z"></path>
+                        <path d="M9 17h6"></path>
+                        <path d="M9 13h6"></path>
+                    </svg>
+                    <p>
+                        Pelaporan
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item mb-2">
+                <a href="{{ route('pelanggaran') }}" class="nav-link {{ Route::is('pelanggaran') ? 'bg-primary' : ''}}">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 icon icon-tabler icon-tabler-file-description"
+                        width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                        fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                        <path d="M14 3v4a1 1 0 0 0 1 1h4"></path>
+                        <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z"></path>
+                        <path d="M9 17h6"></path>
+                        <path d="M9 13h6"></path>
+                    </svg>
+                    <p>
+                        Pelanggaran
+                    </p>
+                </a>
+            </li>
+
+        </ul>
+    </nav>
+    <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+</aside>
